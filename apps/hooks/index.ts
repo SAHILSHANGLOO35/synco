f19d2c328d9addd3ff9eab744689f60f606fd8e4
Sahil Shangloo <sahilshangloo35@gmail.com>
2026-04-14 07:03:33 +0530

    Create POST endpoint /hooks that will put data in the DB and outbox too

--- apps/hooks/index.ts
+++ apps/hooks/index.ts
@@ -1,11 +1,36 @@
 import express from "express"
+import dotenv from "dotenv"
+import { prisma } from "db/client"
 
+dotenv.config()
 const app = express()
+const PORT = process.env.PORT || 3001
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
-  const { userId, zapId } = req.params
+app.post("/hooks/catch/:userId/:syncId", async (req, res) => {
+  const { userId, syncId } = req.params
+  const body = req.body
 
   // store in db a new trigger
+  await prisma.$transaction(async (tx) => {
+    const run = await prisma.syncRun.create({
+      data: {
+        syncId,
+        metadata: body,
+      },
+    })
 
-  // push it on to a queue (kafka/redis)
+    await prisma.syncRunOutbox.create({
+      data: {
+        syncRunId: run.id,
+      },
+    })
+  })
+
+  res.status(200).json({
+    message: "Webhook received",
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`)
 })
