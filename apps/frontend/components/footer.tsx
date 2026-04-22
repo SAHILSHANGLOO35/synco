f0b6576f723cdc1966faf423428d1f6aa1e71ae7
Sahil Shangloo <sahilshangloo35@gmail.com>
2026-04-22 23:34:15 +0530

    Refactor Footer component layout and hide scrollbar for improved aesthetics

--- apps/frontend/components/footer.tsx
+++ apps/frontend/components/footer.tsx
@@ -1,14 +1,28 @@
+import { SquareArrowOutUpRight } from "lucide-react"
 import { Container } from "./container"
 
 export const Footer = () => {
   return (
-    <Container className="pt-16">
-      <div className="flex flex-col gap-6 font-red-hat-display text-white">
-        <div className="font-sans text-xs leading-tight font-medium tracking-wide text-white uppercase md:text-sm">
-          Get started today
+    <Container className="py-8">
+      <div className="flex items-center justify-between px-4 font-red-hat-display text-white">
+        <div className="font-mono text-base font-medium text-neutral-400">
+          @ 2026 SYNCO. All rights reserved.
         </div>
-        <div></div>
-        <div></div>
+        <div className="group flex items-center gap-2 font-mono text-base font-medium text-neutral-400">
+          <span>Created</span>
+          <span>by</span>
+          <a
+            href="https://x.com/doubleSdotdev"
+            target="_blank"
+            className="group flex items-baseline-last gap-1"
+          >
+            <span className="cursor-pointer font-mono text-base font-medium text-neutral-400 transition-all duration-200 ease-in-out group-hover:underline">
+              Sahil
+            </span>
+
+            <SquareArrowOutUpRight size={12} />
+          </a>
+        </div>
       </div>
     </Container>
   )
